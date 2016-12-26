3a6ec6fef4582d29d40b4dbb7839923be95234a3
gg <648628902@qq.com>
2016-12-26 18:50:35 +0800

    Signed-off-by: gg <648628902@qq.com>

--- app/scripts/li.js
+++ app/scripts/li.js
@@ -1,8 +1,8 @@
 angular.module('rulerApp')
  .controller("li",function($scope) {
- 	$scope.data = {"option":[{"title":"","opt":[{"op":"","num":0}],"type":"-1"}],"title":""}
+ 	$scope.data = {"option":[],"title":""}
  	$scope.add = function(){
-		$scope.data.option.push({"title":"","opt":[{"op":"","num":0}],"type":"-1"})
+		$scope.data.option.push({"title":"","opt":[{"op":"","num":0}],"type":"-1","top":[]})
 		//alert(1)
 	}
 	// if($scope.data.option.type == 0||2){
@@ -10,6 +10,13 @@ angular.module('rulerApp')
 	// }else{
 
 	// }
-	
-	
+	$scope.arr=[{"nn":"gg"}]
+	$scope.tian= function(index){
+		$scope.data.option[index].top.push({"nn":"gg"})
+		
+	}
+	$scope.brn= function(index,index2){
+		$scope.data.option[index].top.splice(index2,1)
+		
+	}
  })
