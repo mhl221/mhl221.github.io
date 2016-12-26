9b53d366fbd7908a578de40dd1668880c64cf180
jinbo <1259847246@qq.com>
2016-12-26 16:05:11 +0800

    Signed-off-by: jinbo <1259847246@qq.com>

--- app/scripts/jin.js
+++ app/scripts/jin.js
@@ -1,5 +1,3 @@
-
-
 		document.documentElement.style.fontSize = document.documentElement.clientWidth/320*20+'px';
 		window.onresize = function(){
 			document.documentElement.style.fontSize = document.documentElement.clientWidth/320*20+'px';
