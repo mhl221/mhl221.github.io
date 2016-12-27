eebe9061fdb8e5525d695dfb5ee20436cf2a2210
ylc <422864323@qq.com>
2016-12-27 15:30:07 +0800

    Signed-off-by: ylc <422864323@qq.com>

--- app/scripts/yan.js
+++ app/scripts/yan.js
@@ -1,53 +1,59 @@
-angular.module('rulerApp').filter('paging',function(){      
-  return function(lists,start){
-  	if(lists!==undefined){
-    return lists.slice(start)  
-  	}
-  }
-})
-.controller("yan", ["$scope", "$http","$location","$cookieStore",function($scope, $http,$location,$cookieStore) {
+angular.module('rulerApp').filter('paging', function() {
+		return function(lists, start) {
+			if(lists !== undefined) {
+				return lists.slice(start)
+			}
+		}
+	})
+	.controller("yan", ["$scope", "$http", "$location", "$cookieStore", function($scope, $http, $location, $cookieStore) {
 		$http({
-			url:"http://47.90.20.200:1602/item",
+			url: "http://47.90.20.200:1602/item",
 			method: "get",
-			params:{'uid':$cookieStore.get('uid')}
+			params: {
+				'uid': $cookieStore.get('uid')
+			}
 		}).then(function(e) {
 			console.log(e)
 			$scope.data = e.data;
-			$scope.dataNum =  $scope.data.length;  //获得数据总个数
-    $scope.pages = Math.ceil($scope.dataNum/5);         //按照每页显示3个数据，得到总页数
-    $scope.pageNum = [];                                //生成页码，在 html里 ng-repeat 出来
-    for(var i=0;i<$scope.pages;i++){
-      $scope.pageNum.push(i);
-    }
+			$scope.dataNum = $scope.data.length; 
+			$scope.pages = Math.ceil($scope.dataNum / 10); 
+			$scope.pageNum = []; 
+			for(var i = 0; i < $scope.pages; i++) {
+				$scope.pageNum.push(i);
+			}
 
-    $scope.currentPage = 0;                       //设置当前页是 0
-    $scope.listsPerPage = 5;                      //设置每页显示 3 个
+			$scope.currentPage = 0;
+			$scope.listsPerPage = 10; 
 
-    $scope.setPage = function(num){             // 当点击页码数字时执行的函数
-      $scope.currentPage = num;                 //将当前页 设置为 页码数
-    }
+			$scope.setPage = function(num) { 
+				$scope.currentPage = num; 
+			}
 
-    $scope.prevPage = function(){               //点击上一页执行的函数
-          if($scope.currentPage > 0){
-              $scope.currentPage--;
-          }
-      }
-     $scope.nextPage = function(){              //点击下一页执行的函数
-          if ($scope.currentPage < $scope.pages-1){
-              $scope.currentPage++;
-          }
-      }
-   }, function() {})
-	
-	
-	
-$scope.del = function(index) {
-		alert(index)
-			$scope.data.splice(index, 1)
-		
+			$scope.prevPage = function() { 
+				if($scope.currentPage > 0) {
+					$scope.currentPage--;
+				}
+			}
+			$scope.nextPage = function() { 
+				if($scope.currentPage < $scope.pages - 1) {
+					$scope.currentPage++;
+				}
+			}
+		}, function() {})
+
+		$scope.del = function(e, e2) {
+			$http({
+				url: "http://47.90.20.200:1602/item/" + e.id,
+				method: "delete"
+			}).then(function(cbk) {
+				$scope.result = cbk
+				$scope.data.splice(e2, 1)
+			}, function() {
+
+			})
 		}
-$scope.ck = function(index) {
-//	$location.path('/niez/' + index)
-}
+		$scope.ck = function(index) {
+			//	$location.path('/niez/' + index)
+		}
 
-}])
+	}])
