2b4567814c176c6e30880e846fdb344042a2ac82
ylc <422864323@qq.com>
2016-12-28 20:04:01 +0800

    Merge branch 'master' of https://github.com/mhl221/mhl221.github.io
    # Conflicts:
    #	app/scripts/li.js
    
    Signed-off-by: ylc <422864323@qq.com>

--- app/scripts/yan.js
+++ app/scripts/yan.js
@@ -1,13 +1,57 @@
-angular.module('rulerApp').filter('paging', function() {
-		return function(lists, start) {
-			if(lists !== undefined) {
-				return lists.slice(start)
-			}
+angular.module('rulerApp').filter('fy', function() {
+	return function(e, page, size) {
+		if(e !== undefined) {
+			var start = page * size;
+			var end = (page + 1) * size
+			return e.slice(start, end)
 		}
-	})
-	.controller("yan", ["$scope", "$http", "$location", "$cookieStore", function($scope, $http, $location, $cookieStore) {
 
-		$scope.ylc = function() {
+	}
+}).controller("yan", ["$scope", "$http", "$location", "$cookieStore", function($scope, $http, $location, $cookieStore) {
+	$http({
+		url: "http://47.90.20.200:1602/item",
+		method: "get",
+		params: {
+			'uid': $cookieStore.get('uid')
+		}
+	}).then(function(e) {
+		$scope.data = e.data;
+		$scope.$watch('data', function() {
+			$scope.arr = []
+			for(var i = 0; i < $scope.data.length / 10; i++) {
+				$scope.arr.push(i)
+			}
+			if($scope.page !== 0) {
+				$scope.page = Math.ceil($scope.arr.length / 10)
+			}
+		})
+
+		$scope.page = 0
+		$scope.pages = function(e) {
+
+			$scope.page = e
+		}
+		$scope.prev = function() {
+			$scope.page--
+				if($scope.page < 0) {
+					$scope.page = 0
+				}
+		}
+		$scope.next = function(e) {
+			$scope.page++
+				if($scope.page > $scope.arr.length - 1) {
+					$scope.page = $scope.arr.length - 1
+				}
+		}
+	}, function() {})
+
+	$scope.del = function(e) {
+		$http({
+			url: "http://47.90.20.200:1602/item/" + e.id,
+			method: "delete"
+		}).then(function() {
+			$scope.data.splice($scope.data.indexOf(e), 1)
+
 			$http({
 				url: "http://47.90.20.200:1602/item",
 				method: "get",
@@ -15,46 +59,37 @@ angular.module('rulerApp').filter('paging', function() {
 					'uid': $cookieStore.get('uid')
 				}
 			}).then(function(e) {
-				console.log(e)
 				$scope.data = e.data;
-				$scope.dataNum = $scope.data.length;
-				$scope.pages = Math.ceil($scope.dataNum / 10);
-				$scope.pageNum = [];
-				for(var i = 0; i < $scope.pages; i++) {
-					$scope.pageNum.push(i);
-				}
-
-				$scope.currentPage = 0;
-				$scope.listsPerPage = 10;
-
-				$scope.setPage = function(num) {
-					$scope.currentPage = num;
-				}
-
-				$scope.prevPage = function() {
-					if($scope.currentPage > 0) {
-						$scope.currentPage--;
+				$scope.$watch('data', function() {
+					$scope.arr = []
+					for(var i = 0; i < $scope.data.length / 10; i++) {
+						$scope.arr.push(i)
 					}
-				}
-				$scope.nextPage = function() {
-					if($scope.currentPage < $scope.pages - 1) {
-						$scope.currentPage++;
+					if($scope.page !== 0) {
+						$scope.page = Math.ceil($scope.arr.length / 10)
 					}
+				})
+
+				$scope.page = 0
+				$scope.pages = function(e) {
+
+					$scope.page = e
+				}
+				$scope.prev = function() {
+					$scope.page--
+						if($scope.page < 0) {
+							$scope.page = 0
+						}
+				}
+				$scope.next = function(e) {
+					$scope.page++
+						if($scope.page > $scope.arr.length - 1) {
+							$scope.page = $scope.arr.length - 1
+						}
 				}
 			}, function() {})
-		}
-		$scope.ylc()
-		$scope.del = function(e) {
-			$http({
-				url: "http://47.90.20.200:1602/item/" + e.id,
-				method: "delete"
-			}).then(function() {
-				$scope.data.splice($scope.data.indexOf(e), 1)
-				$scope.ylc()
-			}, function() {})
-		}
-		$scope.ck = function(index) {
-			$location.path('/niez/' + index)
-		}
 
-	}])
+		}, function() {})
+	}
+
+}])
