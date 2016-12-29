ddf18d3cb0e165be03f8b6d05dc2392ec8bc7db9
gg <648628902@qq.com>
2016-12-29 08:54:49 +0800

    Signed-off-by: gg <648628902@qq.com>

--- app/scripts/li.js
+++ app/scripts/li.js
@@ -1,59 +1,4 @@
 angular.module('rulerApp')
-
-	.controller("li", ["$scope", "$http", "$cookieStore", function($scope, $http, $cookieStore) {
-		$scope.data = {
-			"option": [],
-			"title": ""
-		}
-		$scope.add = function() {
-			$scope.data.option.push({
-					"title": "",
-					"opt": [{
-						"op": "",
-						"num": 0
-					}],
-					"type": "",
-					"top": []
-				})
-				//alert(1)
-		}
-		$scope.cct = function(index) {
-				$scope.data.option.splice(index, 1)
-					//alert(1)
-			}
-			// if($scope.data.option.type == 0||2){
-
-		// }else{
-
-		// }
-		$scope.arr = [{
-			"nn": "gg"
-		}]
-		$scope.tian = function(index) {
-			$scope.data.option[index].top.push({
-				"nn": "gg"
-			})
-
-		}
-		$scope.brn = function(index, index2) {
-			$scope.data.option[index].top.splice(index2, 1)
-
-		}
-
-		$scope.acs = function() {
-			$http({
-				url: "http://47.90.20.200:1602/item",
-				method: "post",
-				data: {
-					"uid": $cookieStore.get('uid'),
-					"option": $scope.data.option,
-					"title": $scope.data.title
-				}
-			}).then(function(e) {
-				console.log(e)
-
-				/*if($scope.list.option) {
-
  .controller("li",["$scope","$http","$cookieStore","$location",function($scope,$http,$cookieStore,$location){
  	$scope.data = {"option":[],"title":""}
  	$scope.add = function(){
@@ -91,43 +36,43 @@ angular.module('rulerApp')
 					}).then(function(e) {
 						console.log(e)
 							  $location.path('/gy');
-						/*if($scope.list.option) {
+					// 	if($scope.list.option) {
 
-							for(var j = 0; j < $scope.list.option.length; j++) {
-								if($scope.list.option[j].type == 0) { // 单选题
-									alert(1)
-									$scope.rad.push($scope.list.option[j])
-								} else if($scope.list.option[j].type == 1) { //多选题
-									alert(1)
-									$scope.box.push($scope.list.option[j])
-								} else if($scope.list.option[j].type == 2) { //填空题
-									alert(1)
-									$scope.tex.push($scope.list.option[j])
-								} else { //问答题
-									alert(1)
-									$scope.arear.push($scope.allData.option[j])
-								}
-							}
-						}
-						$("#back").click(function() {
-							$scope.bg = true;
-							$("#box").css('display', "block");
-							$("#xiangxi").css('display', "none");
-							$scope.tex = [];
-							$scope.rad = [];
-							$scope.box = [];
-							$scope.arear = [];
-						})
+					// 		for(var j = 0; j < $scope.list.option.length; j++) {
+					// 			if($scope.list.option[j].type == 0) { // 单选题
+					// 				alert(1)
+					// 				$scope.rad.push($scope.list.option[j])
+					// 			} else if($scope.list.option[j].type == 1) { //多选题
+					// 				alert(1)
+					// 				$scope.box.push($scope.list.option[j])
+					// 			} else if($scope.list.option[j].type == 2) { //填空题
+					// 				alert(1)
+					// 				$scope.tex.push($scope.list.option[j])
+					// 			} else { //问答题
+					// 				alert(1)
+					// 				$scope.arear.push($scope.allData.option[j])
+					// 			}
+					// 		}
+					// 	}
+					// 	$("#back").click(function() {
+					// 		$scope.bg = true;
+					// 		$("#box").css('display', "block");
+					// 		$("#xiangxi").css('display', "none");
+					// 		$scope.tex = [];
+					// 		$scope.rad = [];
+					// 		$scope.box = [];
+					// 		$scope.arear = [];
+					// 	})
 					
-					if($scope.bg) {
-						$("#box").css('display', "none");
-						$("#xiangxi").css('display', "block");
-						$scope.bg = false;
-					} else {
-						$("#box").css('display', "block");
-						$("#xiangxi").css('display', "none");
-						$scope.bg = true;
-					}*/
+					// if($scope.bg) {
+					// 	$("#box").css('display', "none");
+					// 	$("#xiangxi").css('display', "block");
+					// 	$scope.bg = false;
+					// } else {
+					// 	$("#box").css('display', "block");
+					// 	$("#xiangxi").css('display', "none");
+					// 	$scope.bg = true;
+					// }
 			}, function(e) {})
 
 		}
